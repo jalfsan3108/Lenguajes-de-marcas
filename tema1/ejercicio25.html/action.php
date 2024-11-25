538295f3a4fe02d7f5b69e182d93ea6609bc7b74
SrWarfix <jalfsan3108@g.educaand.es>
2024-11-25 17:20:46 +0100

    Update action.php

--- tema1/ejercicio25.html/action.php
+++ tema1/ejercicio25.html/action.php
@@ -3,13 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Datos del Usuario</title>
 </head>
-<body>   
-<p>Bienvenido, usuario: 
-<?php echo "<b>".$_GET["uname"]."</b>";?>
-</p>
-<p>Tu clave es: 
-<?php echo "<b>".$_GET["pass"]."</b>";?></p>
+<body>
+    <h1>Datos recibidos del formulario</h1>
+    <p>Bienvenido, usuario: 
+        <?php 
+            echo "<b>" . htmlspecialchars($_GET["uname"]) . "</b>"; 
+        ?>
+    </p>
+    <p>Tu clave es: 
+        <?php 
+            echo "<b>" . htmlspecialchars($_GET["pass"]) . "</b>"; 
+        ?>
+    </p>
 </body>
-</html> 
+</html>
